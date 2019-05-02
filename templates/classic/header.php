e52e34de1ba8b3baeec5e2dbbc9cde0bb3f321dc
sahil saini <sahil@starclinch.com>
2019-05-02 18:27:32 +0530

    hide header

---
+++ templates/classic/header.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * WCFM plugin templates
+ *
+ * Header area
+ *
+ * @author 		WC Lovers
+ * @package 	wcfm/templates/default
+ * @version   3.1.2
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if( !apply_filters( 'wcfm_is_allow_template_header', true ) ) return;
+?>
+<div id="wcfm-header" class="left-logo">
+	<div class="wcfm-header-container">
+		sahil saini
+	</div>
+</div>
+<div class="wcfm_clearfix"></div>
